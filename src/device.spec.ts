bfb7d0cb529d9e7905fa025c9c2e046b9133cfd0
Brian Leighty <leightyb@teachco.com>
2019-02-12 08:58:50 -0500

    #8: Add support for signed package creation, #9 stop calling home button on deploy, add callback before package is zipped to allow easy modification to the package's contents, add incrementBuildNumber option to enable incrementing build numbers on each build, change outFile to be baseName so we can use for both zip and pkg files, split out device dependent tests into its own device.spect.ts file, move beforeEach/afterEach into describe block to avoid getting outside current file, reenable commented out tests

---
+++ src/device.spec.ts
@@ -0,0 +1,65 @@
+import * as assert from 'assert';
+import * as chai from 'chai';
+import * as chaiFiles from 'chai-files';
+import * as fsExtra from 'fs-extra';
+import * as path from 'path';
+
+import * as rokuDeploy from './index';
+
+chai.use(chaiFiles);
+
+const expect = chai.expect;
+const file = chaiFiles.file;
+
+//these tests are run against an actual roku device. These cannot be enabled when run on the CI server
+describe('device', function () {
+    let options: rokuDeploy.RokuDeployOptions;
+    let originalCwd = process.cwd();
+
+    beforeEach(() => {
+        options = rokuDeploy.getOptions();
+        options.rootDir = './testProject';
+
+    });
+
+    afterEach(() => {
+        //restore the original working directory
+        process.chdir(originalCwd);
+
+        //delete the output file and other interum files
+        let filePaths = [
+            path.resolve(options.outDir),
+            '.tmp'
+        ];
+        for (let filePath of filePaths) {
+            try {
+                fsExtra.removeSync(filePath);
+            } catch (e) { }
+        }
+    });
+
+    this.timeout(20000);
+
+    describe('deploy', function () {
+        it('works', async function () {
+            let response = await rokuDeploy.deploy(options);
+            assert.equal(response.message, 'Successful deploy');
+        });
+
+        it('Presents nice message for 401 unauthorized status code', async function () {
+            options.password = 'NOT_THE_PASSWORD';
+            try {
+                let response = await rokuDeploy.deploy(options);
+                assert.fail('Should have rejected');
+            } catch (e) {
+                assert.equal(e.message, 'Unauthorized. Please verify username and password for target Roku.');
+            }
+        });
+    });
+
+    describe('deployAndSignPackage', () => {
+        it('works', async function () {
+            expect(file(await rokuDeploy.deployAndSignPackage(options))).to.exist;
+        });
+    });
+});
