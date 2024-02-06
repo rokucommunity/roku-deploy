52b1bc06153f67c059cb07a41ed6aa1a0f53ed8f
Milap Naik <milapnaik@gmail.com>
2024-02-06 12:56:47 -0500

    Changed name of toTable

--- src/commands/GetDeviceInfoCommand.ts
+++ src/commands/GetDeviceInfoCommand.ts
@@ -1,10 +1,10 @@
-import { rokuDeploy, toTable } from '../index';
+import { rokuDeploy, printObjectToTable } from '../index';
 
 export class GetDeviceInfoCommand {
     async run(args) {
         const outputPath = await rokuDeploy.getDeviceInfo({
             host: args.host
         });
-        console.log(toTable(outputPath));
+        console.log(printObjectToTable(outputPath));
     }
 }
